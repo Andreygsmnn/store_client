9c33a6bf247e4e86eab6c27484216adf352475cf
Andre <andreygsmnn@gmail.com>
2023-01-25 18:10:01 +0300

    refactor createProduct and store

--- src/components/modalwindow/CreateProduct.js
+++ src/components/modalwindow/CreateProduct.js
@@ -1,102 +1,121 @@
-import React,{useContext, useState,useEffect}from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Context } from '../..'
-import {observer} from 'mobx-react-lite'
-import { createProduct,fetchBrands,fetchTypes } from '../../http/productApi'
+import { observer } from 'mobx-react-lite'
+import { createProduct, fetchBrands, fetchTypes } from '../../http/productApi'
 
 const CreateProduct = observer(() => {
-    const {product} = useContext(Context)
-    const [info, setInfo]=useState([])
-    const [name,setName]= useState('')
-    const [price,setPrice]= useState(0)
-    const [file,setFile] = useState(null)
+    const { product } = useContext(Context)
+    const [info, setInfo] = useState([])
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
 
     useEffect(() => {
         fetchTypes().then(data => product.setTypes(data))
         fetchBrands().then(data => product.setBrands(data))
     }, [])
 
-    const addInfo = (e)=>{
+    const addInfo = (e) => {
         e.preventDefault()
-        setInfo([...info,{title:'', description:'', number: Date.now()}])
+        setInfo([...info, { title: '', description: '', number: Date.now() }])
     }
-    const removeInfo = (number)=>{
-        setInfo(info.filter( i => i.number !== number))
+    const removeInfo = (number) => {
+        setInfo(info.filter(i => i.number !== number))
     }
 
-    const changeInfo = (key,value,number)=>{
-        setInfo(info.map(i => i.number === number ? {...i,[key]:value}: i))
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? { ...i, [key]: value } : i))
     }
-    const selectedFile = e =>{
+    const selectedFile = e => {
         setFile(e.target.files[0])
     }
-    const addProduct = (e)=>{
+    const addProduct = (e) => {
         e.preventDefault()
         const formData = new FormData()
-        formData.append('name',name)
-        formData.append('price',`${price}`)
-        formData.append('img',file)
-        formData.append('brandId',product.selectedBrand.id)
-        formData.append('typeId',product.selectedType.id)
-        formData.append('info',JSON.stringify(info))
-        createProduct(formData).then(data=>console.log(data))
+        formData.append('name', name)
+        formData.append('price', `${price}`)
+        formData.append('img', file)
+        formData.append('brandId', product.selectedBrand.id)
+        formData.append('typeId', product.selectedType.id)
+        formData.append('info', JSON.stringify(info))
+        console.log(product.selectedBrand.id)
+        console.log(product.selectedType.id)
+        console.log(`${price}`)
+        console.log(name)
+        createProduct(formData).then(data => console.log(data))
     }
-  return (
-    <div>
-        <h2>Добавить продукт</h2>
-        <form>
-            <input type="text" 
-            placeholder="Название продукта"
-            value={name}
-            onChange={e=>setName(e.target.value)}
-            />
-            <br/>
-            <input type="number" 
-            placeholder="цена"
-            value={price}
-            onChange={e=>setPrice(Number(e.target.value))}
-            /> 
-            <br/>
-            
-            <input type="file" onChange={selectedFile} />
-            <br/>
-            <input type="text"
-            
-            placeholder="какой бренд"/>  
-            <br/>
-            <input type="text"
-            
-            placeholder="какой тип"/>
-            <br/>
-            <button>Отмена</button>
-            <button onClick={addProduct}>Добавить продукт</button>
-            
-        </form> <br/>
-        <button
-            onClick={addInfo}
+    return (
+        <div>
+            <h2>Добавить продукт</h2>
+            <form>
+                <input type="text"
+                    placeholder="Название продукта"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+                <br />
+                <input type="number"
+                    placeholder="цена"
+                    value={price}
+                    onChange={e => setPrice(Number(e.target.value))}
+                />
+                <br />
+
+                <input type="file" onChange={selectedFile} />
+                <br />
+                <label htmlFor="brands">Выбрать бренд</label>
+
+                <select id="brands" size="5">
+
+                    {product.brands.map(brand => <option
+                        key={brand.id}
+                        onClick={() => product.setSelectedBrand(brand) }
+                    >{brand.name}</option>)}
+
+                </select>
+
+                <br />
+
+                <label htmlFor="types" >Выбрать тип</label>
+                <select id="types" size="5">
+                    {product.types.map(type => <option
+                        key={type.id}
+                        onClick={() => product.setSelectedType(type)}
+                    >{type.name}</option>)}
+                </select>
+
+
+                <br />
+                
+                <button type="submit" onClick={addProduct}>Добавить продукт</button>
+
+            </form> <br />
+            <button
+                onClick={addInfo}
             >Добавить новое свойство</button>
-            
-            {info.map(i => 
+
+            {info.map(i =>
                 <div key={i.number}>
                     <form>
-                        <input type="text" 
-                        placeholder="название свойства"
-                        value={i.title} 
-                        onChange={(e)=>changeInfo('title', e.target.value, i.number)}/><br/>
+                        <input type="text"
+                            placeholder="название свойства"
+                            value={i.title}
+                            onChange={(e) => changeInfo('title', e.target.value, i.number)} /><br />
 
                         <input type="text"
-                        placeholder="описание свойства"
-                        value={i.description}
-                        onChange={(e)=>changeInfo('description', e.target.value, i.number)}
-                        /><br/>
-                        <button>Принять</button><br/>
+                            placeholder="описание свойства"
+                            value={i.description}
+                            onChange={(e) => changeInfo('description', e.target.value, i.number)}
+                        /><br />
+                        <button>Принять</button><br />
                         <button>Отмена</button>
-                        
+
                     </form>
                 </div>)}
-                {/* <button onClick={removeInfo(i.number)}>Удалить свойство</button>  реализовать удаление*/}
-                
-    </div>
-  )
+            {/* <button onClick={removeInfo(i.number)}>Удалить свойство</button>  реализовать удаление*/}
+
+        </div>
+    )
 })
 
 export default CreateProduct
--- src/store/ProductStore.js
+++ src/store/ProductStore.js
@@ -1,46 +1,66 @@
 import { makeAutoObservable } from "mobx"
 
 
-export default class ProductStore{
-    constructor(){
+export default class ProductStore {
+    constructor() {
         this.types = []
         this.brands = []
-        
-        this.products = [
-            // {id:1, name:'Репитер Крокс SW60',price: 16799, rating:4, img:'https://kroks.ru/upload/shop_1/1/6/9/item_1698/item_1698.jpg'},
-            // {id:2, name:'Репитер Vegatel Vt2',price: 123500, rating:1, img:'https://www.vegatel.ru/assets/cache/images/upd_2017/repiter/1800_3g/vt2-1800_3g_led/v1-3-692x562-277.jpg'},
-            // {id:3, name:'Репитер Крокс SW60',price: 16799, rating:2, img:'https://kroks.ru/upload/shop_1/1/6/9/item_1698/item_1698.jpg'},
-            // {id:4, name:'Антенна Vegatel pi',price: 1050, rating:4, img:'https://www.vegatel.ru/assets/cache/images/upd_2017/antena/ant-700_2700-11y/v1-2-692x562-e4d.jpg.webp'},
-            // {id:5, name:'Репитер Крокс SW60',price: 16799, rating:3, img:'https://kroks.ru/upload/shop_1/1/6/9/item_1698/item_1698.jpg'}
-        ]
+        this.products = []
         this.selectedBrand = {}
         this.selectedType = {}
+        this.page = 1
+        this.totalCount = 0
+        this.limit = 3
         makeAutoObservable(this)    // функ mobx следит за изменениями этих переменных и делает ререндер соответ компонентов
     }
 
-    setTypes(types){             //actions геттеры сеттеры
+    setTypes(types) {             //actions геттеры сеттеры
         this.types = types
     }
-    setBrands(brands){
+    setBrands(brands) {
         this.brands = brands
     }
-    setProducts(products){
+    setProducts(products) {
         this.products = products
     }
-    setSelectedType(type){
+    setSelectedType(type) {
+        this.setPage(1)
         this.selectedType = type
     }
-    getTypes(){
-        return this.types
+    setSelectedBrand(brand) {
+        this.setPage(1)
+        this.selectedBrand = brand
     }
-    getBrands(){
-        return this.brands
+    setPage(page) {
+        this.page = page
     }
-    getProducts(){
-        return this.products
+    setTotalCount(count) {
+        this.totalCount = count
     }
-    getSelectedType(){
+    getSelectedType() {
         return this.selectedType
     }
+    getSelectedBrand() {
+        return this.selectedBrand
+    }
+    getTotalCount() {
+        return this.totalCount
+    }
+    getPage(){
+        return this.page
+    }
+    getLimit(){
+        return this.limit
+    }
+    getTypes() {
+        return this.types
+    }
+    getBrands() {
+        return this.brands
+    }
+    getProducts() {
+        return this.products
+    }
     
+
 }
